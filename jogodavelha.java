a88332bed17bbf24f9f3e1761abd0e0afd117f94
Anthony Luciano <102555390+AnthonyLuciano@users.noreply.github.com>
2024-11-14 22:34:05 -0300

    found how JFrame Worked

--- jogodavelha.java
+++ jogodavelha.java
@@ -1,6 +1,25 @@
-public class jogodavelha {
-
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+/*
+ * @author: Anthony Luciano
+ * commit date: 14/11/24
+ */
+public class jogodavelha extends JFrame implements ActionListener {
+    public jogodavelha(){
+        setTitle("Jogo Da Velha");
+        setSize(300, 300);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
+    }
     public static void main(String[] args) {
-        
+        new jogodavelha();
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'actionPerformed'");
     }
 }
