b6a91ae3343c85768f17ffeec93a59d6a59a86cc
Anthony Luciano <102555390+AnthonyLuciano@users.noreply.github.com>
2024-11-14 23:07:43 -0300

    buttons appear now

--- jogodavelha.java
+++ jogodavelha.java
@@ -8,23 +8,27 @@ import java.awt.event.ActionListener;
  */
 public class jogodavelha extends JFrame implements ActionListener {
     private final JButton[][] botoes = new JButton[3][3];
-
     public jogodavelha(){
         //criar a janela java
         setTitle("Jogo Da Velha");
         setSize(300, 300);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
         //gerar os botoes no estilo do jogo
         JPanel painelBotoes = new JPanel();
         painelBotoes.setLayout(new GridLayout(3, 3));
         for(int a = 0; a < 3; a++){
             for(int b = 0; b < 3; b++){
-                //tentando fazer com que os botoes realmente aparecam
-                //tentativa 1- falha
+                //comentarios vao ser menos a partir de agora
                 botoes[a][b] = new JButton("");
+                botoes[a][b].setFont(new Font("Arial", Font.PLAIN, 60));
+                botoes[a][b].setFocusPainted(false);
+                botoes[a][b].addActionListener(this);
+                painelBotoes.add(botoes[a][b]);
             }
         }
+        add(painelBotoes, BorderLayout.CENTER);
+        //movido para ter mais logica e legibilidade
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setVisible(true);
     }
     public static void main(String[] args) {
         new jogodavelha();
